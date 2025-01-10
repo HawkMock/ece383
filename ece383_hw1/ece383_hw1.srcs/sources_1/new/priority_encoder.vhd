4589c2abee7cf5ba03fc5e9c1c34e0bdad7d2008
HawkMock <dustinmock@gmail.com>
2025-01-10 10:43:26 -0700

    Fixed headers

--- ece383_hw1/ece383_hw1.srcs/sources_1/new/priority_encoder.vhd
+++ ece383_hw1/ece383_hw1.srcs/sources_1/new/priority_encoder.vhd
@@ -1,22 +1,53 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 01/10/2025 08:09:09 AM
--- Design Name: 
--- Module Name: priority_encoder - priority_encoder_arch
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
+--+----------------------------------------------------------------------------
+--| 
+--| 2025 United States Air Force Academy
+--| 
+--| United States Air Force Academy     __  _______ ___    _________ 
+--| Dept of Electrical &               / / / / ___//   |  / ____/   |
+--| Computer Engineering              / / / /\__ \/ /| | / /_  / /| |
+--| 2354 Fairchild Drive Ste 2F6     / /_/ /___/ / ___ |/ __/ / ___ |
+--| USAF Academy, CO 80840           \____//____/_/  |_/_/   /_/  |_|
+--| 
+--| ---------------------------------------------------------------------------
+--|
+--| FILENAME      : priority_encoder.vhd
+--| AUTHOR(S)     : C2C Dustin Mock
+--| CREATED       : 2025-01-06
+--| DESCRIPTION   : This file implements a priority encoder. It should output (in binary
+--|                 the index of the leftmost '1' bit of the input.
+--|
+--| DOCUMENTATION : None
+--|
+--+----------------------------------------------------------------------------
+--|
+--| REQUIRED FILES :
+--|
+--|    Libraries : ieee
+--|    Packages  : std_logic_1164, numeric_std, unisim
+--|    Files     : None
+--|
+--+----------------------------------------------------------------------------
+--|
+--| NAMING CONVENSIONS :
+--|
+--|    xb_<port name>           = off-chip bidirectional port ( _pads file )
+--|    xi_<port name>           = off-chip input port         ( _pads file )
+--|    xo_<port name>           = off-chip output port        ( _pads file )
+--|    b_<port name>            = on-chip bidirectional port
+--|    i_<port name>            = on-chip input port
+--|    o_<port name>            = on-chip output port
+--|    c_<signal name>          = combinatorial signal
+--|    f_<signal name>          = synchronous signal
+--|    ff_<signal name>         = pipeline stage (ff_, fff_, etc.)
+--|    <signal name>_n          = active low signal
+--|    w_<signal name>          = top level wiring signal
+--|    g_<generic name>         = generic
+--|    k_<constant name>        = constant
+--|    v_<variable name>        = variable
+--|    sm_<state machine type>  = state machine type definition
+--|    s_<signal name>          = state name
+--|
+--+----------------------------------------------------------------------------
 
 
 library IEEE;
